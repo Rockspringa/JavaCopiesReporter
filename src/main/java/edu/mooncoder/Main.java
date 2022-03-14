692041f6ac35c3521eec41d9733caa3c6c9e5977
Dylan <dylan.daev@gmail.com>
2022-03-13 22:23:42 -0600

    initted rml

--- src/main/java/edu/mooncoder/Main.java
+++ src/main/java/edu/mooncoder/Main.java
@@ -2,6 +2,6 @@ package edu.mooncoder;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hola mundo BOF");
+        System.out.println("Hola mundo RML");
     }
 }
